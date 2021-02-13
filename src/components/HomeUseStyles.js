743ff9fe1d2102107eb17166b364da54b885a057
HBDUDE <supersuniiside@gmail.com>
2021-02-13 16:52:31 -0700

    Moved the old home code to its own file.

---
+++ src/components/HomeUseStyles.js
@@ -0,0 +1,42 @@
+const useStyles = makeStyles(theme => ({
+  "@global": {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: "none"
+    }
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`
+  },
+  toolbar: {
+    flexWrap: "wrap"
+  },
+  toolbarTitle: {
+    flexGrow: 1
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+    textTransform: "capitalize"
+  },
+  linkButton: {
+    margin: theme.spacing(1, 1.5),
+    textTransform: "capitalize",
+    fontFamily: "Cabin",
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6)
+  },
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up("sm")]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6)
+    }
+  }
+}));
+
+export default useStyles
