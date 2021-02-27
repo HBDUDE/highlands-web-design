dfbaa591be16e7df7492f9292021028a93f88cac
HBDUDE <supersuniiside@gmail.com>
2021-02-26 22:27:56 -0700

    Styled text inputs

--- src/components/HomeUseStyles.js
+++ src/components/HomeUseStyles.js
@@ -23,7 +23,7 @@ const useStyles = makeStyles(theme => ({
   footer: {
     borderTop: `1px solid ${theme.palette.divider}`,
     marginTop: theme.spacing(2),
-    paddingTop: theme.spacing(3),
+    paddingTop: theme.spacing(1),
     paddingBottom: theme.spacing(3),
     [theme.breakpoints.up("sm")]: {
       marginTop: theme.spacing(1),
